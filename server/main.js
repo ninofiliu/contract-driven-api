a1d62358d70b3d7f38d6c3f12c76c67773a86697
ninofiliu <nino.filiu@gmail.com>
2020-03-31 00:20:31 +0200

    use router

--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./router');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(router);
 
 app.listen(8081, () => console.log('Server listening http://localhost:8081'));
