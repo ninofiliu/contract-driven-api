01566b91037031ae5133f4ef6d23eef5862a61da
ninofiliu <nino.filiu@gmail.com>
2020-03-31 19:39:46 +0200

    fix users module

--- server/users.module.js
+++ server/users.module.js
@@ -2,9 +2,13 @@ const db = require('./db');
 
 module.exports = {
   exists(partialUser) {
-    const found = db.get('users')
-      .find((user) => !partialUser.username || partialUser.username === user.username)
-      .find((user) => !partialUser.password || partialUser.password === user.password)
+    const found = db
+      .get('users')
+      .find((user) => {
+        if (partialUser.username && partialUser.username !== user.username) return false;
+        if (partialUser.password && partialUser.password !== user.password) return false;
+        return true;
+      })
       .value();
     return !!found;
   },
