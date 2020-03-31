74dfddf6c5d77117163a46982c1c15425b7522d7
ninofiliu <nino.filiu@gmail.com>
2020-03-31 19:09:40 +0200

    JoinSignup

--- client/components/JoinSignup.vue
+++ client/components/JoinSignup.vue
@@ -1,15 +1,15 @@
 <template>
   <form @submit.prevent="submit">
     <div v-if="message">{{ message }}</div>
-    <div><input v-model="username" placeholder="username"></div>
-    <div><input v-model="password" placeholder="password"></div>
-    <div><button type="submit">[signup]</button></div>
+    <div><input v-model="username" placeholder="Username"></div>
+    <div><input v-model="password" placeholder="Password"></div>
+    <div><button type="submit">[Signup]</button></div>
   </form>
 </template>
 
 <script>
-// import api from '../api';
-// import ValidationError from '../api/ValidationError';
+import api from '../api';
+import ValidationError from '../api/ValidationError';
 
 export default {
   data() {
@@ -20,8 +20,15 @@ export default {
     };
   },
   methods: {
-    submit() {
-      // TODO
+    async submit() {
+      try {
+        const { error } = await api.post('/auth/signup', { username: this.username, password: this.password });
+        if (error) return this.message = error;
+        this.message = 'Successful signup';
+      } catch (e) {
+        if (e instanceof ValidationError) return this.message = 'Password must be > 4 char';
+        throw e;
+      }
     },
   },
 };
