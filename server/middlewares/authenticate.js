79586e664e2a284b611b7950acbb0c6ec3f4481d
ninofiliu <nino.filiu@gmail.com>
2020-03-31 21:19:10 +0200

    authenticate mw

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -1,4 +1,7 @@
+/** @type {import('express').RequestHandler} */
 module.exports = (req, res, next) => {
-  console.log('authenticate - not implemented yet');
+  const token = req.get('Authorization');
+  if (!token) return res.status(401).send('Authorization header must contain token');
+  if (token !== 'ssshhh') return res.status(403).send('Invalid token');
   next();
 };
